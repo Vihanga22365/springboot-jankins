8dd7b05531996083347fc6c93accc3ed9a41ec7e
Vihanga Mihiranga <50793729+Vihanga22365@users.noreply.github.com>
2023-06-21 09:58:54 +0530

    Initial Commit

--- src/main/java/com/virtusa/vihanga/springbootjankins/controller/Site.java
+++ src/main/java/com/virtusa/vihanga/springbootjankins/controller/Site.java
@@ -13,4 +13,9 @@ public class Site {
     public String getMessage(@PathVariable("name") String name) {
         return "Welcome to the Assignment System " + name;
     }
+
+    @GetMapping("welcome")
+    public String displayDetails() {
+        return "Hi Application";
+    }
 }
