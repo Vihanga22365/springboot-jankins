597db2c85ed65f5ceb90237e61610938b21563f1
Vihanga Mihiranga <50793729+Vihanga22365@users.noreply.github.com>
2023-06-21 10:02:26 +0530

    Initial Commit

--- src/main/java/com/virtusa/vihanga/springbootjankins/SpringbootJankinsApplication.java
+++ src/main/java/com/virtusa/vihanga/springbootjankins/SpringbootJankinsApplication.java
@@ -3,15 +3,22 @@ package com.virtusa.vihanga.springbootjankins;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
 @Slf4j
-public class SpringbootJankinsApplication {
+public class SpringbootJankinsApplication extends SpringBootServletInitializer {
 
     public static void main(String[] args) {
         log.info(".......................Start Vihanga App............................ ");
         SpringApplication.run(SpringbootJankinsApplication.class, args);
         log.info(".......................After Vihanga App............................. ");
+    }
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        return builder.sources(SpringbootJankinsApplication.class);
     }
 
 }
