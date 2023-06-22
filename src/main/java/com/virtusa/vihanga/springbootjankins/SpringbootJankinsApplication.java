cc99f03aa74c31b79bda7c22d1e3ed814ce14294
Vihanga Mihiranga <50793729+Vihanga22365@users.noreply.github.com>
2023-06-22 21:17:28 +0530

    try to fix bug in deployment

--- src/main/java/com/virtusa/vihanga/springbootjankins/SpringbootJankinsApplication.java
+++ src/main/java/com/virtusa/vihanga/springbootjankins/SpringbootJankinsApplication.java
@@ -2,12 +2,19 @@ package com.virtusa.vihanga.springbootjankins;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class SpringbootJankinsApplication {
+public class SpringbootJankinsApplication extends SpringBootServletInitializer {
 
     public static void main(String[] args) {
         SpringApplication.run(SpringbootJankinsApplication.class, args);
+    }
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        return builder.sources(SpringbootJankinsApplication.class);
     }
 
 }
