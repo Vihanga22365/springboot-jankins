8d32302896609b45abdba274a7d9850aadd305cc
Vihanga Mihiranga <50793729+Vihanga22365@users.noreply.github.com>
2023-06-22 08:59:43 +0530

    try to fix bug in deployment

--- src/main/java/com/virtusa/vihanga/springbootjankins/controller/Site.java
+++ src/main/java/com/virtusa/vihanga/springbootjankins/controller/Site.java
@@ -1,12 +1,10 @@
 package com.virtusa.vihanga.springbootjankins.controller;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("api/v1")
+@CrossOrigin(origins = "*")
 public class Site {
 
     @GetMapping("/message/{name}")
