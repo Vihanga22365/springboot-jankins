c930650742350f1a6c7446eed89f71c9b001b3c6
Vihanga Mihiranga <50793729+Vihanga22365@users.noreply.github.com>
2023-06-22 20:50:41 +0530

    try to fix bug in deployment

--- src/main/java/com/virtusa/vihanga/springbootjankins/SpringbootJankinsApplication.java
+++ src/main/java/com/virtusa/vihanga/springbootjankins/SpringbootJankinsApplication.java
@@ -5,13 +5,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-@Slf4j
 public class SpringbootJankinsApplication {
 
     public static void main(String[] args) {
-        log.info(".......................Start Vihanga App............................ ");
         SpringApplication.run(SpringbootJankinsApplication.class, args);
-        log.info(".......................After Vihanga App............................. ");
     }
 
 }
